3ebbc6113eda7409a7b792973d0d6d16326ae069
Artyom Galay <artemijgalaj@gmail.com>
2023-06-23 22:36:23 +0600

    New backend service

--- core/axios.ts
+++ core/axios.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { parseCookies } from "nookies";
 
-<<<<<<< HEAD
+
+
 axios.defaults.baseURL = "https://flux-backnd-11a895dcfc01.herokuapp.com/";
-=======
-axios.defaults.baseURL = "https://flux-backnd-11a895dcfc01.herokuapp.com//";
->>>>>>> ac6f19b97b6f873087f22c6a7dc6330469222988
+
 
 axios.interceptors.request.use((config) => {
 	if (typeof window !== "undefined") {
